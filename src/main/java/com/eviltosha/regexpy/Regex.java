6bcb1e1efee9cb1baeac26135f9409d6f7fa4e26
Anton Samoylov <toshaevil@gmail.com>
2014-06-26 14:09:17 +0400

    Refactoring to remove unnecessary class and field

--- src/main/java/com/eviltosha/regexpy/Regex.java
+++ src/main/java/com/eviltosha/regexpy/Regex.java
@@ -1,11 +1,7 @@
 package com.eviltosha.regexpy;
 
-import com.eviltosha.regexpy.RegexSyntaxException;
-
-import java.lang.Exception;
 import java.lang.Override;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 /**
  * Hello world!
@@ -13,11 +9,14 @@ import java.util.Iterator;
  */
 public class Regex
 {
+  // FIXME: should it be encapsulated to own class?
+  private Node myStartNode;
+
   // FIXME: are there default setters?
   public Regex(String regex) {
-    myRegex = regex;
-    myGraph = new Graph();
-    myGraph.construct(regex);
+    myStartNode = new EmptyNode();
+    Node endNode = new EndNode();
+    construct(myStartNode, endNode, regex);
   }
 
   // FIXME: which modifiers (private/..., static, etc) should apply to inner classes?
@@ -31,7 +30,7 @@ public class Regex
     }
 
     abstract int matchPart(String str, int strPos);
-    boolean isFinal() { return false; }
+    boolean isEnd() { return false; }
     void addNextNode(Node node) {
       myNextNodes.add(node);
       // FIXME: debug output, remove before deployment
@@ -52,10 +51,10 @@ public class Regex
     void print() { System.out.println("Empty node"); }
   }
 
-  static class FinalNode extends EmptyNode {
+  static class EndNode extends EmptyNode {
     // FIXME: is it ok to override for this particular behavior?
     @Override
-    boolean isFinal() { return true; }
+    boolean isEnd() { return true; }
   }
 
   static class SymbolNode extends Node {
@@ -76,87 +75,71 @@ public class Regex
     void print() { System.out.println("Symbol node: " + mySymbol); }
   }
 
-  static class Graph extends Object {
-    Node myStart = new EmptyNode();
-
-    // FIXME: should we pass myRegex, or use it directly from here?
-    void construct(String regex) {
-      int pos = 0;
-      Node curNode = myStart;
-      while (pos < regex.length()) {
-        char nextChar = regex.charAt(pos);
-        // FIXME: google style guide for switches (and find all other switches in the code)
-        switch (nextChar) {
-          case '*':
-            // fall through
-          case '+':
-            // FIXME: is this the correct way to throw exceptions?
-            throw new RegexSyntaxException("Incorrect use of quantifier", regex);
-          default:
-            Node newNode = new SymbolNode(nextChar);
-            newNode.print();
-            curNode.addNextNode(newNode);
-            ++pos;
-            // FIXME: code dubbing, possibly extract or refactor
-            // FIXME: is it ok to reuse same var for different purpose?
-            if (pos < regex.length()) {
-              nextChar = regex.charAt(pos);
-              switch (nextChar) {
-                case '*':
-                  newNode.addNextNode(newNode);
-                  Node newEmptyNode = new EmptyNode();
-                  newNode.addNextNode(newEmptyNode);
-                  curNode.addNextNode(newEmptyNode);
-                  curNode = newEmptyNode;
-                  ++pos;
-                  break;
-                case '+':
-                  newNode.addNextNode(newNode);
-                  ++pos;
-                  // fall through
-                default:
-                  // we don't increment pos here, because we'll process this char next in the loop
-                  curNode = newNode;
-              }
-            } else {
-              curNode = newNode;
+  private void construct(Node startNode, Node endNode, String regex) {
+    int pos = 0;
+    Node curNode = startNode;
+    while (pos < regex.length()) {
+      char nextChar = regex.charAt(pos);
+      // FIXME: google style guide for switches (and find all other switches in the code)
+      switch (nextChar) {
+        case '*':
+          // fall through
+        case '+':
+          // FIXME: is this the correct way to throw exceptions?
+          throw new RegexSyntaxException("Incorrect use of quantifier", regex);
+        default:
+          Node newNode = new SymbolNode(nextChar);
+          newNode.print();
+          curNode.addNextNode(newNode);
+          ++pos;
+          // FIXME: code dubbing, possibly extract or refactor
+          // FIXME: is it ok to reuse same var for different purpose?
+          if (pos < regex.length()) {
+            nextChar = regex.charAt(pos);
+            switch (nextChar) {
+              case '*':
+                newNode.addNextNode(newNode);
+                Node newEmptyNode = new EmptyNode();
+                newNode.addNextNode(newEmptyNode);
+                curNode.addNextNode(newEmptyNode);
+                curNode = newEmptyNode;
+                ++pos;
+                break;
+              case '+':
+                newNode.addNextNode(newNode);
+                ++pos;
+                // fall through
+              default:
+                // we don't increment pos here, because we'll process this char next in the loop
+                curNode = newNode;
             }
-        }
+          } else {
+            curNode = newNode;
+          }
       }
-      Node finalNode = new FinalNode();
-      curNode.addNextNode(finalNode);
     }
-
-    boolean match(String str) {
-      // FIXME: this is quite dirty hack, should bypass it
-      return match(str, 0, myStart);
-    }
-
-    // FIXME: is it ok to override (or how's it called?) with same name but different signature and number of parameters?
-    boolean match(String str, int pos, Node curNode) {
-      if (pos == str.length() && curNode.isFinal()) {
-        return true;
-      }
-      // the case (pos == str.length() - 1) and curNode isn't final will be processed below
-      if (pos > str.length()) {
-        return false;
-      }
-
-      // FIXME: is it ok to get internal field without getter?
-      for (Node nextNode: curNode.getNextNodes()) {
-        int increment = nextNode.matchPart(str, pos);
-        if (increment > -1 && match(str, pos + increment, nextNode)) {
-          return true;
-        }
-      }
-      return false;
-    }
+    curNode.addNextNode(endNode);
   }
 
   public boolean match(String str) {
-    return myGraph.match(str);
+    return match(str, 0, myStartNode);
   }
 
-  private Graph myGraph;
-  private String myRegex;
+  private boolean match(String str, int pos, Node curNode) {
+    if (pos == str.length() && curNode.isEnd()) {
+      return true;
+    }
+    // the case (pos == str.length() - 1) and curNode isn't final will be processed below
+    if (pos > str.length()) {
+      return false;
+    }
+
+    for (Node nextNode: curNode.getNextNodes()) {
+      int increment = nextNode.matchPart(str, pos);
+      if (increment > -1 && match(str, pos + increment, nextNode)) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
