9e21e5c949fd7b387b0e6d6369522c3a663cf0ab
Anton Samoylov <toshaevil@gmail.com>
2014-07-01 17:09:27 +0400

    fix exception

--- src/main/java/com/eviltosha/regexpy/RegexSyntaxException.java
+++ src/main/java/com/eviltosha/regexpy/RegexSyntaxException.java
@@ -1,14 +1,11 @@
 package com.eviltosha.regexpy;
 
-/**
- *
- */
 public class RegexSyntaxException extends IllegalArgumentException {
   // TODO: this exception should indicate position of error in string
   private final String myDesc;
   private final String myRegex;
 
-  public RegexSyntaxException(String desc, String regex) {
+  public RegexSyntaxException(final String desc, final String regex) {
     myDesc = desc;
     myRegex = regex;
   }
