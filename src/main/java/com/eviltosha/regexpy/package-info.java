e0d936d1815cec92cbcf99a42d3094723e91ea50
Anton Samoylov <toshaevil@gmail.com>
2014-07-01 17:29:53 +0400

    add basic usage

--- src/main/java/com/eviltosha/regexpy/package-info.java
+++ src/main/java/com/eviltosha/regexpy/package-info.java
@@ -2,4 +2,10 @@ package com.eviltosha.regexpy;
 
 /** * Provides classes Regex and Matcher for matching strings against regular expressions. Regex class
  stores a compiled representation of the regular expression. Several Matchers for a single Regex object
- can exist and operate at the same time */
+ can exist and operate at the same time.
+
+ Basic usage:
+ Regex regex = new Regex("foo|bar");
+ Matcher matcher = regex.matcher();
+ boolean matches = matcher.matches("foo");
+ */
