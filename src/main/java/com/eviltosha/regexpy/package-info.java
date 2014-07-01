bccb1fdb581a931a0f70060c6a0f311ad3c52d0f
Anton Samoylov <toshaevil@gmail.com>
2014-07-01 17:04:25 +0400

    package info

---
+++ src/main/java/com/eviltosha/regexpy/package-info.java
@@ -0,0 +1,5 @@
+package com.eviltosha.regexpy;
+
+/** * Provides classes Regex and Matcher for matching strings against regular expressions. Regex class
+ stores a compiled representation of the regular expression. Several Matchers for a single Regex object
+ can exist and operate at the same time */
